c81e021f7959e96bedbf98f05aafe8496eca1858
Anirban <anirbansharma36@gmail.com>
2024-06-11 12:25:55 +0530

    11/06/2024 - [ UI updated ! expression from foodDisplay fixed ] (#02)

--- frontend/src/components/fooddisplay/FoodDisplay.jsx
+++ frontend/src/components/fooddisplay/FoodDisplay.jsx
@@ -10,13 +10,13 @@ const FoodDisplay = ({ category }) => {
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes Near You</h2>
-      {!loading && (
+      {loading && (
         <div className="gif-bike-parent">
           <img className="gif-bike" src={gif} alt={'loading...'} />
           <h4>Loading...</h4>
         </div>
       )}
-      {loading && (
+      {!loading && (
         <div className="food-display-list">
           {food_list.map((item, index) => {
             if (category === "All" || category === item.category) {
